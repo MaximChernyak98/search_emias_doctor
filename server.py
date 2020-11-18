65b3a68dc50fcb0c6db4ebf3625cca630947d591
spels_wsl2 <maximchernyak98@gmail.com>
2020-11-18 19:23:02 +0300

    get current weather

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
 from flask import Flask
 
+from weather import weather_by_city
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Хола!'
+def index():
+    weather = weather_by_city('Moscow, Russia')
+    if weather:
+        result_string = f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
+        return result_string
+    else:
+        return 'Сервис погоды не работает'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
