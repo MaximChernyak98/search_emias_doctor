7723bd5e6172da32e14ad719f605f3c41e772658
mecher_acer <maximchernyak98@gmail.com>
2020-11-18 22:54:29 +0300

    add html output

--- server.py
+++ server.py
@@ -9,10 +9,17 @@ app = Flask(__name__)
 def index():
     weather = weather_by_city('Moscow, Russia')
     if weather:
-        result_string = f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
-        return result_string
+        weather_string = f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
     else:
-        return 'Сервис погоды не работает'
+        weather_string = 'Сервис погоды не работает'
+    return f"""<html>
+        <head>
+            <title>Прогноз погоды</title>
+        </head>
+        <body>
+        <h1>{weather_string}</h1>
+        </body>
+    </html>"""
 
 
 if __name__ == '__main__':
